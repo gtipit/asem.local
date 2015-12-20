05a36133a0d045243d43eee96ad9a02bf6b2ac67
ztipit@desktip <tipit@ukr.net>
2015-12-20 13:38:05 +0200

    Начало ... или продолжение...

---
+++ asem.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from asem import asem
+asem.debug = True
+if __name__ == "__main__":
+    #asem.run(port=5555, host='0.0.0.0')
+    asem.run()
+
+
--- asem/views/views.py
+++ asem/views/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from asem import asem
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, make_response
+from flask import Flask, __version__, request, session, g, redirect
+from flask import  url_for, abort, render_template, flash, make_response
 
 
 @asem.route('/')
 @asem.route('/index')
 def index():
-    return "Hello, Asem! <br> Flask version - %s" % Flask.__version__
+    return "Hello, Asem! <br> Flask version - %s" % __version__
 ###
 
 
